f95a8fcd7848fcf64112beaeb88dd9799def4f49
Zach Bruggeman <mail@bruggie.com>
2019-06-03 10:54:55 -0400

    Clean up and format code with prettier

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-module.exports = function (uri) {
-    var mime   = uri.split(',')[0].split(':')[1].split(';')[0];
-    var bytes  = atob(uri.split(',')[1]);
-    var len    = bytes.length;
-    var buffer = new window.ArrayBuffer(len);
-    var arr    = new window.Uint8Array(buffer);
+const toBlob = function(uri) {
+  const mime = uri
+    .split(',')[0]
+    .split(':')[1]
+    .split(';')[0];
+  const bytes = atob(uri.split(',')[1]);
+  const len = bytes.length;
+  const buffer = new window.ArrayBuffer(len);
+  const arr = new window.Uint8Array(buffer);
 
-    for (var i = 0; i < len; i++) {
-        arr[i] = bytes.charCodeAt(i);
-    }
+  for (let i = 0; i < len; i++) {
+    arr[i] = bytes.charCodeAt(i);
+  }
 
-    return new Blob([arr], { type: mime });
-}
+  return new Blob([arr], { type: mime });
+};
+
+const init = function() {
+  if (!module.exports.supported) return;
+  const CanvasPrototype = window.HTMLCanvasElement.prototype;
+
+  if (!CanvasPrototype.toBlob && CanvasPrototype.toDataURL) {
+    CanvasPrototype.toBlob = function(callback, type, quality) {
+      callback(toBlob(this.toDataURL(type, quality)));
+    };
+  }
+};
 
 // IE >= 10, most modern browsers
 // The Blob type can't be polyfilled, which is why there aren't any polyfills for TypedArrays for older IE's
-module.exports.supported = (
-    typeof window.HTMLCanvasElement !== 'undefined' &&
-    typeof window.atob !== 'undefined' &&
-    typeof window.Blob !== 'undefined' &&
-    typeof window.ArrayBuffer !== 'undefined' &&
-    typeof window.Uint8Array !== 'undefined'
-);
+const supported =
+  typeof window.HTMLCanvasElement !== 'undefined' &&
+  typeof window.atob !== 'undefined' &&
+  typeof window.Blob !== 'undefined' &&
+  typeof window.ArrayBuffer !== 'undefined' &&
+  typeof window.Uint8Array !== 'undefined';
 
-module.exports.init = function () {
-    if (!module.exports.supported) return;
-    var CanvasPrototype = window.HTMLCanvasElement.prototype;
-    
-    if (!CanvasPrototype.toBlob && CanvasPrototype.toDataURL) {
-        CanvasPrototype.toBlob = function (callback, type, quality) {
-            callback(module.exports(this.toDataURL(type, quality)));
-        }
-    }
-}
+module.exports = toBlob;
+module.exports.init = init;
+module.exports.supported = supported;
